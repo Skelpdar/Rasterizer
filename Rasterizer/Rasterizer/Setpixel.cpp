39421e14f02846a2035fe9c441874b1dd01ab3c2
Skelpdar <skelpdar@gmail.com>
2016-09-02 13:46:12 +0200

    Fixed crash when setting pixels outside the screen boundaries

--- Rasterizer/Rasterizer/Setpixel.cpp
+++ Rasterizer/Rasterizer/Setpixel.cpp
@@ -3,8 +3,10 @@
 void Setpixel(SDL_Surface* surf, int x, int y, 
 	Color col,int pitch, int bpp) {
 
-	Uint8* p = (Uint8*)surf->pixels + y*pitch + x*bpp;
-	*(p) = col.B;
-	*(p + 1) = col.G;
-	*(p + 2) = col.R;
+	if (x >= 0 && x < surf->w && y >= 0 && y < surf->h) {
+		Uint8* p = (Uint8*)surf->pixels + y*pitch + x*bpp;
+		*(p) = col.B;
+		*(p + 1) = col.G;
+		*(p + 2) = col.R;
+	}
 }
