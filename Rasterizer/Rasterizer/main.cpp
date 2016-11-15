d7ce4daf1e86b68fc17da8d828ddbde1b260b6f4
Skelpdar <skelpdar@gmail.com>
2016-11-15 13:19:36 +0100

    Fixed error that only allowed to render one model in the scene

--- Rasterizer/Rasterizer/main.cpp
+++ Rasterizer/Rasterizer/main.cpp
@@ -26,6 +26,7 @@ int main(int, char**) {
 		return 1;
 	}
 
+	//Pixel buffer
 	SDL_Surface* surface = SDL_GetWindowSurface(window);
 
 	int bpp = surface->format->BytesPerPixel;
@@ -38,62 +39,25 @@ int main(int, char**) {
 	rasterizer.setBpp(bpp);
 	rasterizer.setSurface(surface);
 
-	std::vector<Model*> modellist;
-
-	//garbage
-
-	Vertex v1(1.0, 1.0, 1.0);
-	Vertex v2(1.0, -1.0, 1.0);
-	Vertex v3(-1.0, 1.0, 1.0);
-	Vertex v4(-1.0, -1.0, 1.0);
-
-	std::vector<Vertex*> face;
-	face.push_back(&v3);
-	face.push_back(&v2);
-	face.push_back(&v1);
-
-	Face f1(face);
-
-	face.clear();
-	face.push_back(&v3);
-	face.push_back(&v4);
-	face.push_back(&v2);
-
-	Color red(255, 0, 0, 0);
-	f1.setColor(red);
-
-	Face f2(face);
-
-	std::vector<Face*> faces;
-	faces.push_back(&f1);
-	faces.push_back(&f2);
-
-	Model model(faces);
-
-	model.position.matrix.clear();
-	model.position.matrix.push_back(0);
-	model.position.matrix.push_back(0);
-	model.position.matrix.push_back(5);
-
-	model.scale.matrix.clear();
-	model.scale.matrix.push_back(1);
-	model.scale.matrix.push_back(1);
-	model.scale.matrix.push_back(1);
-
-	model.rotation.matrix.clear();
-	model.rotation.matrix.push_back(0);
-	model.rotation.matrix.push_back(0);
-	model.rotation.matrix.push_back(0);
-
+	//Scene initialization
+	Model model;
 	model.loadFromFile("model.txt");
+	model.position = Matrix(0, 0, 20);
+	model.scale = Matrix(1, 1, 1);
 
+	Model model2;
+	model2.loadFromFile("model.txt");
+	Matrix position(5, 0, 20);
+	model2.position = position;
+	model2.scale = Matrix(1, 1, 1);
+
+	std::vector<Model*> modellist;
 	modellist.push_back(&model);
+	modellist.push_back(&model2);
 
 	Camera camera;
 	camera.position = Matrix(0, 0, 0);
 	camera.rotation = Matrix(0, 0, 0);
-
-	// end of garbage
 
 	bool runProgram = true;
 
@@ -101,7 +65,6 @@ int main(int, char**) {
 
 	float frameduration = 0;
 
-	//Program loop TODO
 	//Rendering
 	while (runProgram){
 		
@@ -142,15 +105,15 @@ int main(int, char**) {
 
 			//Setting up the individual matrices
 			Matrix4 translate;
-			translate.initialize_translation(model.position.matrix[0], model.position.matrix[1], model.position.matrix[2]);
+			translate.initialize_translation((*modeliter)->position.matrix[0], model.position.matrix[1], model.position.matrix[2]);
 			Matrix4 scale;
-			scale.initialize_scaling(model.scale.matrix[0], model.scale.matrix[1], model.scale.matrix[2]);
+			scale.initialize_scaling((*modeliter)->scale.matrix[0], model.scale.matrix[1], model.scale.matrix[2]);
 			Matrix4 rotatex;
-			rotatex.initialize_rotate_x(model.rotation.matrix[0]);
+			rotatex.initialize_rotate_x((*modeliter)->rotation.matrix[0]);
 			Matrix4 rotatey;
-			rotatey.initialize_rotate_y(model.rotation.matrix[1]);
+			rotatey.initialize_rotate_y((*modeliter)->rotation.matrix[1]);
 			Matrix4 rotatez;
-			rotatez.initialize_rotate_z(model.rotation.matrix[2]);
+			rotatez.initialize_rotate_z((*modeliter)->rotation.matrix[2]);
 			Matrix4 cameratrans;
 			cameratrans = camera.getTranslationMatrix();
 			Matrix4 cameraxrot;
@@ -203,8 +166,8 @@ int main(int, char**) {
 		SDL_Delay( 5 );
 
 		//model.rotation.matrix[0] += 1;
-		//model.rotation.matrix[1] += 1;
-		model.rotation.matrix[2] += 1;
+		model.rotation.matrix[1] += 1;
+		//model.rotation.matrix[2] += 1;
 
 		//runProgram = false;
 		
